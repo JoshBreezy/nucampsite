7a495230c7312a3e33ab1c98a9813bc108047288
Josh Brazinski <jmbrazinski@gmail.com>
2022-03-22 14:24:39 -0700

    redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -36,7 +36,7 @@ function RenderComments({ comments }) {
             );
           })
         }
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -61,9 +61,8 @@ class CommentForm extends Component {
   };
 
   handleSubmit(values) {
-    alert(`Current state is ${JSON.stringify(values)}`);
-    console.log(`Current state is ${JSON.stringify(values)}`)
     this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   };
 
   render() {
@@ -119,7 +118,10 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id} />
         </div>
       </div>
     );
